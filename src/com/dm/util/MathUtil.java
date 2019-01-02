0540db0561d84baa6bcf38829e30cf10daea677b
Dmitry M <di_mi@mail.ru>
2019-01-02 18:02:14 +0300

    Refactor MathUtil

--- src/com/dm/util/MathUtil.java
+++ src/com/dm/util/MathUtil.java
@@ -5,6 +5,7 @@ public class MathUtil {
     static public double sum(double a1, double a2){
         return a1+a2;
     }
+
     static public double diff(double a1, double a2) {
         return a1-a2;
     }
