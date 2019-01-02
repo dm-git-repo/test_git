8f982310c7cf471cf35a90364bb402607ede484e
Dmitry M <di_mi@mail.ru>
2019-01-02 17:40:54 +0300

    Stop using sum

--- src/com/dm/Main.java
+++ src/com/dm/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-//        System.out.println("Sum 5 + 3 is " + MathUtil.sum(5.0,3.0));
+//         System.out.println("Sum 5 + 3 is " + MathUtil.sum(5.0,3.0));
     }
 }
 
