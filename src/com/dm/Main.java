74e4dd300316a7d00b3404f882442841e166ee13
Dmitry M <di_mi@mail.ru>
2019-01-02 19:05:46 +0300

    Update Main from Eclipse

--- src/com/dm/Main.java
+++ src/com/dm/Main.java
@@ -5,7 +5,7 @@ import com.dm.util.MathUtil;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!");
         System.out.println("Sum 5 + 3 is " + MathUtil.sum(5.0,3.0));
         System.out.println("Sum 5 + 3 is " + MathUtil.diff(5.0,3.0));
     }
