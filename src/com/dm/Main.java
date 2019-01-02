1b7979d7df2c8416e9426b00418e722b737b9d51
Dmitry M <di_mi@mail.ru>
2019-01-02 17:33:56 +0300

    Main use sum mrthod

--- src/com/dm/Main.java
+++ src/com/dm/Main.java
@@ -1,10 +1,12 @@
 package com.dm;
 
+import com.dm.util.MathUtil;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hell all!");
+        System.out.println("Sum 5 + 3 is " + MathUtil.sum(5.0,3.0));
     }
 }
 
--- src/com/dm/util/MathUtil.java
+++ src/com/dm/util/MathUtil.java
@@ -2,7 +2,7 @@ package com.dm.util;
 
 public class MathUtil {
 
-    public double sum(double a1, double a2){
+    static public double sum(double a1, double a2){
         return a1+a2;
     }
 
