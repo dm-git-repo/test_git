5e4003a5daf7225dd1a9e95957d8f5f7ccd42f6a
Dmitry M <di_mi@mail.ru>
2019-01-02 17:50:09 +0300

    Add diff

--- src/com/dm/util/MathUtil.java
+++ src/com/dm/util/MathUtil.java
@@ -5,5 +5,8 @@ public class MathUtil {
     static public double sum(double a1, double a2){
         return a1+a2;
     }
+    static public double diff(double a1, double a2) {
+        return a1-a2;
+    }
 
 }
